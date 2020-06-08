d19b050cd1fa3439dcf0cf4ec14cefb0a51cb34b
unknown <jeanhern81@gmail.com>
2020-06-08 12:25:03 -0700

    optimized for mobile view

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,15 @@
-
-// If you can see 'Hello, World!' in the console, then you can remove this and replace it your code
-console.log('Hello, World!');
-// YOUR JS CODE GOES HERE
-// only needing core
+//Moment.js
 //date math
-(function()
-{
+(function() {
   // instantiate a moment object
   var NowMoment = moment();
-  
   // instantiate a JavaScript Date object
   var NowDate = new Date();
-  
   // display value of moment object in #displayMoment div
   var eDisplayMoment = document.getElementById('displayMoment');
   eDisplayMoment.innerHTML = NowMoment;
-  
   // display value of Date object in #displayJsDate div
   var eDisplayDate = document.getElementById('displayJsDate');
   eDisplayDate.innerHTML = NowDate;
 })();
+
